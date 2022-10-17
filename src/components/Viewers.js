55e47d65924e2b884923a5765448b151a7202d08
sachin-source <sachinmgvt@gmail.com>
2022-10-17 20:38:51 +0530

    added viewers and images

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -5,11 +5,21 @@ import styled from 'styled-components'
 const Viewers = () => {
   return (
     <Container>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
+        <Wrap>
+          <img src='/images/viewers-disney.png' alt='' />
+        </Wrap>
+        <Wrap>
+          <img src='/images/viewers-pixar.png' alt='' />
+        </Wrap>
+        <Wrap>
+          <img src='/images/viewers-marvel.png' alt='' />
+        </Wrap>
+        <Wrap>
+          <img src='/images/viewers-starwars.png' alt='' />
+        </Wrap>
+        <Wrap>
+          <img src='/images/viewers-national.png' alt='' />
+        </Wrap>
     </Container>
   )
 }
@@ -26,5 +36,30 @@ grid-template-columns: repeat(5, minmax(0, 1fr));
     grid-template-columns: repeat(1, minmax(0, 1fr));
 }
 `
+
+const Wrap = styled.div`
+padding-top: 56.25%;
+border-radius: 10px;
+box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+            rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+cursor: pointer;
+overflow: hidden;
+position : relative;
+transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+border: 3px solid rgba(249, 249, 249, 0.1);
+
+img {
+  inset: 0px;
+  display: block;
+  height : 100%;
+  object-fit: cover;
+  opacity:1;
+  position: absolute;
+  transition: opacity 500ms ease-in-out 0s;
+  width: 100%;
+  z-index: 1;
+  top: 0;
+}
+`;
 
 export default Viewers
